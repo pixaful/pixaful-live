1334308f1479a3cc8b12d3232cabce3ac09b573d
Hayden <hello@haydentomas.com>
2019-08-05 22:11:17 +0100

    switch to tipu slide

---
+++ site/static/js/tipueslide_content.js
@@ -0,0 +1,15 @@
+
+var tipueslide = {"pages": [
+     {"title": "Tipue", "text": "Tipue is a small web development studio based in North London.", "url": "http://www.tipue.com"},
+     {"title": "Tipue Search, a jQuery Site Search Plugin", "text": "Tipue Search is a site search jQuery plugin. It's free, open source, responsive and fast.", "img": "./img/surface.jpg", "url": "http://www.tipue.com/search/"},
+     {"title": "Tipue Slide, a Sliding Panel Site Search jQuery Plugin", "text": "Tipue Slide is a sliding panel site search jQuery plugin. It's free, open source and fast.", "img": "./img/screens.jpg", "url": "http://www.tipue.com/slide/"},
+     {"title": "Tipr, a Small and Simple jQuery Tooltip Plugin", "text": "Tipr is a jQuery tooltip plugin. It works on almost any element, and it's free and open source.", "img": "./img/glow.jpg", "url": "http://www.tipue.com/tipr/"},
+     {"title": "About Tipue", "text": "Tipue is a small web development studio based in North London, founded in 2001. We design innovative and original jQuery plugins and add-in code. We talk to MySQL and similar with heavy-duty Perl.", "url": "http://www.tipue.com/is/"},     
+     {"title": "Playing with CSS Sticky Positioning", "text": "Blog. While the fixed and sticky position elements seems rather similar, the sticky element maintains its position only within its containing block. Sticky is relative.", "img": "./img/sony.jpg", "url": "http://www.tipue.com/blog/css-sticky/"},
+     {"title": "The Complete Guide to Centering a Div", "text": "Blog. Every developer inevitably finds that centering a div isn't as obvious as you'd expect. Centering what's inside a div horizontally is easy but then things tend to get a bit sticky. When you get to centering a div vertically, you can end up in a world of CSS hurt.", "img": "./img/boom.jpg", "url": "http://www.tipue.com/blog/center-a-div/"},
+     {"title": "Cool CSS Radio Buttons and Checkboxes", "text": "Blog. Hip and easy radio button and checkbox form elements styled with CSS.", "img": "./img/amp.jpg", "url": "http://www.tipue.com/blog/radio-checkbox/"},
+     {"title": "Hacking CSS Writing Mode", "text": "Blog. The writing-mode CSS property sets horizontal and vertical text direction. While meant for multilingual purposes, it can be exploited for design.", "img": "./img/tran.jpg", "url": "http://www.tipue.com/blog/css-writing-mode/"},
+     {"title": "CSS Animated Pulse Buttons using Box Shadow", "text": "Blog. On-trend pulse buttons using CSS animations, keyframes and box-shadow.", "url": "http://www.tipue.com/blog/css-pulse-buttons/"},
+     {"title": "Cool Select Boxes with CSS and jQuery", "text": "Blog. Cool, hip select elements styled with CSS and jQuery.", "url": "http://www.tipue.com/blog/select-box/"},
+     { "title": "Hayden Tomas", "text": "Hayden Tomas built this website using HUGO, a static site builder than can build thousands of pages in less than a second.", "tags": "Hayden, web developer, pixaful", "note": "<a href='http://pixaful.dev/blog/'>More articles</a>", "url": "http://pixaful.dev/about" }
+]};
