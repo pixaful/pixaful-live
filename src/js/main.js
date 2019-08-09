e6a3cea2ddc13fba749ebb7d940530f5595276ce
hayden Tomas <hello@haydentomas.com>
2019-08-09 14:57:35 +0100

    testing concat js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
-
+//@prepros-append jquery.min.js
+//@prepros-append uikit.min.js
+//@prepros-append uikit-icons.min.js
 //@prepros-append jquery.smartmenus.min.js
+//@prepros-append tipueslide_content.js
+
 
 
