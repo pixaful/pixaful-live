f66276587116addf05856917445c3994896d69d6
Hayden <hello@haydentomas.com>
2019-08-10 18:44:47 +0100

    cookiebot

--- src/js/tipueslide_content.js
+++ src/js/tipueslide_content.js
@@ -1,7 +1,7 @@
 
 var tipueslide = {"pages": [
 {"title": "What's up with static site generators?", "text": "Blog. So we are in the process of moving our portfolio website to Hugo. Having seen what Smashing Magazine achieved, we wanted a piece of that. Plus it gives us the chance to brush up on our development skills.", "img": "./uploads/hugo-design.jpg", "url": "/blog/static-site-generators/"},
-{"title": "E-Coomerce Development", "text": "Blog. So we are in the process of moving our portfolio website to Hugo. Having seen what Smashing Magazine achieved, we wanted a piece of that. Plus it gives us the chance to brush up on our development skills.", "img": "./uploads/hugo-design.jpg", "url": "http://www.tipue.com/blog/css-writing-mode/"},
+{"title": "E-Commerce Development", "text": "Blog. So we are in the process of moving our portfolio website to Hugo. Having seen what Smashing Magazine achieved, we wanted a piece of that. Plus it gives us the chance to brush up on our development skills.", "img": "./uploads/hugo-design.jpg", "url": "http://www.tipue.com/blog/css-writing-mode/"},
 
 
 ]};
